cf11ff1c108c5b9092d7bc40529d3ec6ea2860b1
dalyyy3259@protonmail.com <dalyyy3259@protonmail.com>
2024-05-21 11:49:58 +0100

    flow nextjs

---
+++ src/app/api/test/route.js
@@ -0,0 +1,5 @@
+export async function GET(request) {
+
+
+    return Response.json({ product: "this is my product" })
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 
 import Profile from "@/components/Profile";
 export default function Home() {
+  const data = fetch("http://localhost:3000/api/test").then(res => res.json()).then(data => console.log(data, "hereeeeee"))
+
   return (
     <><Profile /></>
 
